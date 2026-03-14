82d774761dbc935838d18eef6dded0fe60574f6e
rss96-ua <123976590+rss96-ua@users.noreply.github.com>
2026-03-14 15:51:57 +0100

    475856555N - Create CADProduct and ENProduct, modified Default

---
+++ library/ENProduct.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class ENProduct
+    {
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+        public string Code{
+            get
+            {
+                return _code;
+            }
+
+            set
+            {
+                _code = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Amount
+        {
+            get
+            {
+                return _amount;
+            }
+
+            set
+            {
+                _amount = value;
+            }
+        }
+
+        public float Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                _price = value;
+            }
+        }
+
+        public int Category
+        {
+            get
+            {
+                return _category;
+            }
+
+            set
+            {
+                _category = value;
+            }
+        }
+
+        public DateTime CreationDate
+        {
+            get
+            {
+                return _creationDate;
+            }
+
+            set
+            {
+                _creationDate = value;
+            }
+        }
+
+        public ENProduct()
+        {
+
+        }
+
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Amount = amount;
+            this.Category = category;
+            this.CreationDate = creationDate;
+
+        }
+
+        public bool Create()
+        {
+            try
+            {
+
+
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool Update()
+        {
+            try
+            {
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete()
+        {
+            try
+            {
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool Read()
+        {
+            try
+            {
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool ReadFirst()
+        {
+            try
+            {
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool ReadNext()
+        {
+            try
+            {
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool ReadPrev()
+        {
+            try
+            {
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
