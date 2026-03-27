42fca6258815c3e96813d195317b992f8794eeac
Pablo García <pgl66@alu.ua.es>
2026-03-27 09:10:27 +0100

    23900722L CADCategory y ENCategory

---
+++ library/CADCategory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace library {
+    public class CADCategory {
+        private string constring;
+
+        public CADCategory() {
+            constring = ConfigurationManager.ConnectionStrings["hada-p3"].ConnectionString;
+        }
+
+        public bool Read(ENCategory en) {
+            try {
+                using (SqlConnection con = new SqlConnection(constring)) {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(
+                        "SELECT id, name FROM Categories WHERE id=@id", con);
+                    cmd.Parameters.AddWithValue("@id", en.Id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read()) {
+                        en.Id = (int)reader["id"];
+                        en.Name = reader["name"].ToString();
+                        return true;
+                    }
+                    return false;
+                }
+            } catch (SqlException ex) {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public List<ENCategory> ReadAll() {
+            List<ENCategory> list = new List<ENCategory>();
+            try {
+                using (SqlConnection con = new SqlConnection(constring)) {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(
+                        "SELECT id, name FROM Categories ORDER BY id ASC", con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read()) {
+                        ENCategory cat = new ENCategory();
+                        cat.Id = (int)reader["id"];
+                        cat.Name = reader["name"].ToString();
+                        list.Add(cat);
+                    }
+                }
+            } catch (SqlException ex) {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return list;
+        }
+    }
+}
---
+++ library/ENCategory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace library {
+    public class ENCategory {
+        private int _id;
+        private string _name;
+
+        public int Id { get { return _id; } set { _id = value; } }
+        public string Name { get { return _name; } set { _name = value; } }
+
+        public ENCategory() { }
+
+        public ENCategory(int id, string name) {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public bool Read() {
+            try { return new CADCategory().Read(this); } catch { return false; }
+        }
+
+        public List<ENCategory> ReadAll() {
+            try { return new CADCategory().ReadAll(); } catch { return new List<ENCategory>(); }
+        }
+    }
+}
